57fe498227a8eca1633a413678778a0956f57af1
Giovanni <GiovanniGiampaolo@users.noreply.github.com>
2020-12-23 10:21:08 +0100

    Updates

---
+++ precache-manifest.f9df6ba0284b57e62174d3540a997fe9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8029d5e6e650afb7468c175a0ba66a2",
+    "url": "/slideshow-app/index.html"
+  },
+  {
+    "revision": "ac8a5e7db6a9daf3ad80",
+    "url": "/slideshow-app/static/css/4.4932a258.chunk.css"
+  },
+  {
+    "revision": "979b722f3daa3ea4d935",
+    "url": "/slideshow-app/static/css/main.e43691d7.chunk.css"
+  },
+  {
+    "revision": "ac8a5e7db6a9daf3ad80",
+    "url": "/slideshow-app/static/js/4.9b80b728.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/slideshow-app/static/js/4.9b80b728.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49a785a24c9bbcf16c3d",
+    "url": "/slideshow-app/static/js/5.dfb9c74f.chunk.js"
+  },
+  {
+    "revision": "90b6b156a5f62810eddc9189ae0e43d4",
+    "url": "/slideshow-app/static/js/5.dfb9c74f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8db3ee45056257d4b9a4",
+    "url": "/slideshow-app/static/js/6.53ebfd6b.chunk.js"
+  },
+  {
+    "revision": "979b722f3daa3ea4d935",
+    "url": "/slideshow-app/static/js/main.2f43ec3d.chunk.js"
+  },
+  {
+    "revision": "f6e7843c3468957be925",
+    "url": "/slideshow-app/static/js/runtime-main.66c5aaa7.js"
+  },
+  {
+    "revision": "abd053df4ff34bf7f2a9",
+    "url": "/slideshow-app/static/js/stencil-h8k-logo_2-entry-js.c6c63472.chunk.js"
+  },
+  {
+    "revision": "656dc23b9482aeb2c67b",
+    "url": "/slideshow-app/static/js/stencil-polyfills-css-shim.aeec233e.chunk.js"
+  }
+]);
